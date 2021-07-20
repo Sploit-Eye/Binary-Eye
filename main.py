9c201bb3d5bae4065bdbaee0011fdacc3f8b80f6
Pawandeep Singh <87510878+Sploit-Eye@users.noreply.github.com>
2021-07-20 11:59:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 # Text to Binary
 # Convert your text into binary format.
 
@@ -12,9 +14,15 @@ print(" Version:", version   )
 print("---------------------")
 print("")
 
-text = input("Type your text: ") # Type your text
+rtools = input("Python is Installed [Yes/No] : ")
 print("")
 
-binary = "".join(format(ord(i), "08b") for i in text)
-
-print("Binary:", binary) # Prints text in binary format.
+if (rtools == "Yes"):
+    text = input("Type your text: ") # Type your text
+    print("")
+    binary = "".join(format(ord(i), "08b") for i in text)
+    print("Binary:", binary) # Prints text in binary format.
+elif(rtools == "No"):
+    os.system("bash requirements.sh")
+else:
+    print("Please type [Yes/No] only.")
