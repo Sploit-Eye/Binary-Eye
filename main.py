518cea21864d4ee8713a7ea56323e01f58dc571d
Pawandeep Singh <87510878+Sploit-Eye@users.noreply.github.com>
2021-07-20 11:15:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 # Text to Binary
 # Convert your text into binary format.
 
+print("""
+
+█░█░█ █▀▀ █░░ █▀▀ █▀█ █▀▄▀█ █▀▀
+▀▄▀▄▀ ██▄ █▄▄ █▄▄ █▄█ █░▀░█ ██▄
+
+""")
+
 text = input("Type your text: ") # Type your text
+print("")
 
 binary = "".join(format(ord(i), "08b") for i in text)
 
