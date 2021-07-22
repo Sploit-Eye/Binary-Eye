83f96de487e4d56a092882fbb812eb30a05aa116
Pawandeep Singh <sploiteye@gmail.com>
2021-07-22 15:47:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,24 +5,37 @@ import os
 
 version = "be-1.0-se"
 
-print("""
+def greencolor(skk): print("\033[92m {}\033[00m" .format(skk))
+def redcolor(skk): print("\033[91m {}\033[00m" .format(skk))
+
+greencolor("""
 █▄▄ █ █▄░█ ▄▀█ █▀█ █▄█   █▀▀ █▄█ █▀▀
 █▄█ █ █░▀█ █▀█ █▀▄ ░█░   ██▄ ░█░ ██▄
 """)
-print("---------------------")
-print(" Version:", version   )
-print("---------------------")
+print("[0] Install required tools")
+print("[1] Start Binary Eye")
+print("[2] About")
+print("[3] Exit")
 print("")
 
-rtools = input("Is Python3 Installed [Yes/No] : ")
-print("")
+uanswer = input("Choose: ")
 
-if (rtools == "Yes"):
-    text = input("Type your text: ") # Type your text
-    print("")
-    binary = "".join(format(ord(i), "08b") for i in text)
-    print("Binary:", binary) # Prints text in binary format.
-elif(rtools == "No"):
+if (uanswer == "0"):
     os.system("bash requirements.sh")
+elif(uanswer == "1"):
+    print("")
+    ntext = input("Type your text: ")
+    print("")
+    bformat = "".join(format(ord(i), "08b")for i in ntext)
+    print("Binary code:", bformat)
+elif(uanswer == "2"):
+    greencolor("Binary Eye is created by Sploit Eye.")
+    greencolor("This tool converts your text into binary format, this tool")
+    greencolor("is open source. Source code is available on GitHub.")
+    print("")
+    greencolor("Source code - https://github.com/Sploit-Eye/binary-eye")
+elif(uanswer == "3"):
+    exit()
 else:
-    print("Please type [Yes/No] only.")
+    print("")
+    redcolor("Please type the given numbers.")
