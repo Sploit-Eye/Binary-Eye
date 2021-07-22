d4a5d4c650fe53efcdba734ec1eb65917f0751c1
Pawandeep Singh <sploiteye@gmail.com>
2021-07-22 15:47:20 +0530

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -2,26 +2,26 @@
 # Run this file first to avoid errors in main.py
 # This script will install all required tools for main.py
 clear
-echo """
+tput setaf 2; echo """
 █▀█ █▀▀ █▀█ █░█ █ █▀█ █▀▀ █▀▄▀█ █▀▀ █▄░█ ▀█▀ █▀
 █▀▄ ██▄ ▀▀█ █▄█ █ █▀▄ ██▄ █░▀░█ ██▄ █░▀█ ░█░ ▄█
 """
+tput sgr0;
 echo ""
-sleep 2
+sleep 1.5
 echo "- Checking required tools..."
 sudo apt install python3 -y # Installing Python3
 clear
 python3 --version # Check Python version
-sleep 3
-clear
-echo ""
+sleep 1.6
 clear
 echo "- Required tools are installed."
-sleep 1.9
-clear
+sleep 1.1
 echo "- Running Binary Eye..."
-sleep 2.1
+sleep 0.4
+echo "- Script is running..."
+sleep 0.6
 clear
-sleep 2.3
+sleep 1
 python3 main.py # Runs python script [Binary-Eye]
 exit 1
