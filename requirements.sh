2f844384fbe534ed24ae95cad00fe188fe352d9c
Pawandeep Singh <sploiteye@gmail.com>
2021-07-25 08:46:29 +0530

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -6,33 +6,33 @@ tput setaf 2; echo """
 █▀█ █▀▀ █▀█ █░█ █ █▀█ █▀▀ █▀▄▀█ █▀▀ █▄░█ ▀█▀ █▀
 █▀▄ ██▄ ▀▀█ █▄█ █ █▀▄ ██▄ █░▀░█ ██▄ █░▀█ ░█░ ▄█
 """
-echo "be-1.3-se"
 tput sgr0;
+echo "be-1.4-se"
 echo ""
-sleep 1.5
+sleep 1.0
 echo "- Checking required tools..."
 sudo apt install python3 -y # Installing Python3
 sudo apt install ncurses-utils -y # Installing Ncurses-utils for output colors
 clear
 tput setaf 2;
 printf "-" && python3 --version # Check Python version
-sleep 1.4
+sleep 0.6
 printf "-" && tput -V # Check Ncurses-utils version
-sleep 1.4
+sleep 0.6
 tput sgr0;
 clear
-tput setaf 1;
+tput setaf 2;
 echo "- Required tools are installed."
 tput sgr0;
-sleep 1.1
-tput setaf 1;
+sleep 0.3
+tput setaf 2;
 echo "- Running Binary Eye..."
-sleep 0.4
+sleep 0.1
 tput sgr0;
 tput setaf 2;
 echo "- Script is running..."
 tput sgr0;
-sleep 0.6
+sleep 0.1
 clear
 sleep 1
 python3 main.py # Runs python script [Binary-Eye]
